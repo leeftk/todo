982e6162609cd6cf019837458929a8467bbcce2b
leeftk <40748420+leeftk@users.noreply.github.com>
2018-10-08 02:59:13 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+//Check off by clicking
+$("ul").on("click", "li",function(){
+	$(this).toggleClass("completed")
+})
+//Click on X
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove()
+	})
+	event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+		if(event.which === 13){
+			var todotext = $(this).val() 
+			$(this).val('')
+			$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todotext + "</li>")
+		}
+	});
+$("#pus").click(function(){
+	$("input[type='text']").fadeToggle()
+	});
